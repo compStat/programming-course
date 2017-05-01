d7140470d676da0fef3c456095bc3d391f498dab
Sebastian <wahani@gmail.com>
2017-05-01 13:15:05 +0200

    initial commit

---
+++ scriptDayOne.R
@@ -0,0 +1,30 @@
+################################################################################
+# Das Skript zum Entwickeln der Übungen und Beispiele während der Schulung:    #
+#   Programmieren mit R.                                                       #
+#                                                                              #
+# Datum 02.05.2017                                                             #
+#                                                                              #
+# Autoren: Steffen Wagner / Sebastian Warnholz                                 #
+# E-mail: [steffen.wagner|sebastian.warnholz]@inwt-statistics.de               #
+################################################################################
+
+
+# 00 Vorbereitung --------------------------------------------------------------
+
+# Leeren des Workspace
+rm(list = ls(all.names = TRUE))
+
+# Verwendete Pakete
+library(ggplot2)
+
+# Globale Variablen
+A_GLOBAL_VARIABLE <- 4
+
+
+# 01a Laden von Daten ----------------------------------------------------------
+
+
+# 01b Aufbereitung von Daten ---------------------------------------------------
+
+
+# 02 ... -----------------------------------------------------------------------
